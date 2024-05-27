9f9535bcbf84c0e9d1aed838432e02af84ff3a7b
adityaisingh <ishu9918663060@gmail.com>
2024-05-27 16:13:20 +0530

    update server url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://blog-app-61ix.onrender.com",
         secure: false,
       },
     },
